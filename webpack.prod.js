174e37d309dfacc5bf5e51be21be00378c83579c
Siti Rahmah <strahmasti@gmail.com>
2024-04-22 17:11:08 +0800

    First commit

---
+++ webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+const config = require('./webpack.config');
+
+module.exports = merge(config, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
