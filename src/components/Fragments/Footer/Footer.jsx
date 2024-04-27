8232b92b0ace9bf25bc1adb98ebe74c37310345b
Aqil Rahmatullah <aqilr54@gmail.com>
2024-04-27 13:36:29 +0800

    add: footer bar

--- src/components/Fragments/Footer/Footer.jsx
+++ src/components/Fragments/Footer/Footer.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWhatsapp,
+  faInstagram,
+  faXTwitter,
+  faFacebook,
+  faTiktok,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
-    <footer className="absolute bottom-0 w-full h-[350px] flex flex-col items-center bg-primary-100 py-[27px] px-[108px]">
-      <div className="flex w-full justify-between">
+    <footer className="absolute bottom-0 w-full h-fit flex flex-col items-center bg-primary-100 pt-[27px] px-[108px] font-sans">
+      <div id="footer1" className="flex w-full justify-between py-[27px]">
         <div className="w-[480px] flex flex-col gap-3">
           <img
             src="images/tukaratik-logo.png"
@@ -21,9 +29,70 @@ function Footer() {
             </p>
           </div>
         </div>
-        <div className="w-[415px] flex flex-col gap-[10px] pt-[38px] pb-[10px] items-start px-[10px]"></div>
+        <div className="w-[415px] flex flex-col gap-6 pt-[38px] pb-[10px] items-start px-[10px]">
+          <div className="flex flex-col items-start gap-[7px]">
+            <h1 className="text-Title text-primary-700">Layanan Kami</h1>
+            <div className="flex w-full gap-6">
+              <div className="flex items-center gap-[10px] flex-row-reverse">
+                <a
+                  href="index"
+                  className="text-Subtitle font-bold text-tertiary-600 hover:text-tertiary-800 active:text-tertiary-900 peer"
+                >
+                  Kirim Limbah
+                </a>
+                <div className="w-[2px] h-[8px] bg-primary-700 ml-[1px] peer-hover:bg-primary-800 peer-active:bg-primary-900"></div>
+              </div>
+              <div className="flex items-center gap-[10px] flex-row-reverse">
+                <a
+                  href="index"
+                  className="text-Subtitle font-bold text-tertiary-600 hover:text-tertiary-800 active:text-tertiary-900 peer"
+                >
+                  Tukar Point
+                </a>
+                <div className="w-[2px] h-[8px] bg-primary-700 ml-[1px] peer-hover:bg-primary-800 peer-active:bg-primary-900"></div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-[7px] items-start">
+            <h1 className="text-Title text-primary-700">Kantor Pusat</h1>
+            <div className="text-left flex flex-col gap-1">
+              <p className="text-Subtitle font-bold text-primary-700">
+                Buka Setiap Selasa - Minggu 09:00 - 17:30 WITA
+              </p>
+              <p className="text-Subtitle text-primary-700 text-pretty">
+                Duta Mall Banjarmasin, Jl. Ahmad Yani KM 2, Banjarmasin,
+                Kalimantan Selatan
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div></div>
+      <div
+        id="footer2"
+        className="w-[1200px] border-t border-primary-600 flex gap-[37px] px-5 py-4 justify-center"
+      >
+        <h1 className="text-Title font-bold text-primary-700 py-[10px]">
+          Temukan kami untuk pertanyaan, saran, atau kolaborasi atau kunjungi
+          sosial media kami melalui
+        </h1>
+        <div className="flex items-center gap-[10px] text-primary-700">
+          <a href="fb.com">
+            <FontAwesomeIcon icon={faWhatsapp} className="social-icon" />
+          </a>
+          <a href="fb.com">
+            <FontAwesomeIcon icon={faInstagram} className="social-icon" />
+          </a>
+          <a href="fb.com">
+            <FontAwesomeIcon icon={faXTwitter} className="social-icon" />
+          </a>
+          <a href="fb.com">
+            <FontAwesomeIcon icon={faFacebook} className="social-icon" />
+          </a>
+          <a href="fb.com">
+            <FontAwesomeIcon icon={faTiktok} className="social-icon" />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
