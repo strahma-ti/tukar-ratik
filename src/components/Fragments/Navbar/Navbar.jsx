e8db2c0a359aaa015577e1bbbf2679a5fc46e478
Aqil Rahmatullah <aqilr54@gmail.com>
2024-04-28 14:57:01 +0800

    add: navbar

---
+++ src/components/Fragments/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Avatar, Dropdown } from "flowbite-react";
+
+function Navbar() {
+  return (
+    <nav className="w-full h-[66px] px-7 bg-white flex items-center justify-between shadow-500 font-sans">
+      <img
+        src="images/app-logo-m.png"
+        alt=""
+        className="w-[158px] h-[46px] object-cover"
+      />
+      <div className="flex items-center gap-10">
+        <a
+          href="/"
+          className="text-Subtitle font-semibold text-primary-700 hover:text-primary-800"
+        >
+          Kirim Limbah
+        </a>
+        <a
+          href="/"
+          className="text-Subtitle font-semibold text-primary-700 hover:text-primary-800"
+        >
+          Tukar Poin
+        </a>
+      </div>
+      <Dropdown
+        label={
+          <div className="flex items-center">
+            <span className="ml-2 text-Subtitle font-bold text-primary-700 hover:text-primary-800">
+              Masuk
+            </span>
+          </div>
+        }
+        arrowIcon={false}
+        inline
+      >
+        <Dropdown.Item>Login</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item>Register</Dropdown.Item>
+      </Dropdown>
+    </nav>
+  );
+}
+
+export default Navbar;
