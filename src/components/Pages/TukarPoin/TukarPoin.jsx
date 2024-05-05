da69b7b4431d0c725be08353359c56d9bcd3c28e
Aqil Rahmatullah <aqilr54@gmail.com>
2024-05-06 00:23:56 +0800

    add: search product and product list section in tukar poin page

--- src/components/Pages/TukarPoin/TukarPoin.jsx
+++ src/components/Pages/TukarPoin/TukarPoin.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navbar from '../../Fragments/Navbar/Navbar';
 import HeroSection from '../../Layout/TukarPoin/HeroSection';
+import SearchProductSection from '../../Layout/TukarPoin/SearchProductSection';
+import ProductListSection from '../../Layout/TukarPoin/ProductListSection';
+import Footer from '../../Fragments/Footer/Footer';
 
 function TukarPoin() {
   return (
@@ -8,10 +11,15 @@ function TukarPoin() {
       <Navbar />
       <div
         id="main-content"
-        className="flex flex-col items-center justify-center mt-10"
+        className="flex flex-col items-center justify-center mt-10 font-sans"
       >
         <HeroSection />
+        <div className="mt-[85px] flex flex-col gap-[50px]">
+          <SearchProductSection />
+          <ProductListSection />
+        </div>
       </div>
+      <Footer />
     </>
   );
 }
